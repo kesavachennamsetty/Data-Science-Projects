366b6edebbe6fc719f6bcfc17f7832ebcda8292a
kesava <manichennamsetty06@gmail.com>
2024-09-20 11:29:20 +0530

    added utilities

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
-    ''' This function will return the list of requirements
+    '''
+    this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_path.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
 setup(
